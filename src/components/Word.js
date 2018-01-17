539be75b31d9352e767dd6253a825429df008981
Programmer <ser.dmitri@gmail.com>
2018-01-17 09:58:53 -0500

    Make the Word a Component class

--- src/components/Word.js
+++ src/components/Word.js
@@ -5,26 +5,50 @@ import Children from './Children'
 
 import '../static/css/Word.css'
 
-const Word = ({ 
-  spelling,
-  pronounciation,
-  definition,
-  derivation=void(0),
-  children=[],
-  siblings=[]}) =>
-  <article className="word">
-    <Siblings siblings={siblings}/>
-    <article className="row main">
-      <Spelling spelling={spelling}/>
-      <section className="pronounciation">
-        {pronounciation}
-      </section>
-    </article>
-    <section className="derivation row">
-      {derivation}
-    </section>
-    <Definition definition={definition}/>
-    <Children children={children}/>
-  </article>
+import { Component } from 'react'
+import PropTypes from 'prop-types'
 
-export default Word
+export default class Word extends Component {
+  render() {
+    const {
+      spelling,
+      pronounciation,
+      definition,
+      derivation,
+      children,
+      siblings
+    } = this.props
+
+    return (
+      <article className="word">
+        <Siblings siblings={siblings}/>
+        <article className="row main">
+          <Spelling spelling={spelling}/>
+          <section className="pronounciation">
+            {pronounciation}
+          </section>
+        </article>
+        <section className="derivation row">
+          {derivation}
+        </section>
+        <Definition definition={definition}/>
+        <Children children={children}/>
+      </article>
+    )
+  }
+}
+
+Word.propTypes = {
+  spelling: PropTypes.array.isRequired,
+  pronounciation: PropTypes.string.isRequired,
+  definition: PropTypes.array.isRequired,
+  derivation: PropTypes.string,
+  children: PropTypes.array,
+  siblings: PropTypes.array
+}
+
+Word.defaultProps = {
+  derivation: "",
+  children: [],
+  siblings: []
+}
