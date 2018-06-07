07ea8810f1702ac0ee33b065a4876fc0c56aa1cd
programmer <ser.dmitri@gmail.com>
2018-06-07 09:06:23 -0400

    No need to inject any state to WordCreate

--- src/components/WordCreate/WordCreate.js
+++ src/components/WordCreate/WordCreate.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
 
 import WordCreateForm from './WordCreateForm'
 import WordSubmitButton from './WordSubmitButton'
 
-export class WordCreate extends Component {
-  componentWillReceiveProps(nextProps) {
-    const { form } = nextProps
-    if (form.values !== this.props.form.values) {
-      console.log("FORM CHANGED", form.values);    
-    }
-  }
+export default class WordCreate extends Component {
 
   render () {
     return (
@@ -22,13 +14,3 @@ export class WordCreate extends Component {
     )
   }
 }
-
-WordCreate.propTypes = {
-  form: PropTypes.object.isRequired
-}
-
-function mapStateToProps({form}) {
-  return { form }
-}
-
-export default connect(mapStateToProps)(WordCreate)
